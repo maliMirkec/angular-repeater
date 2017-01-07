e58490600db3466cca06eef6fd6c1cbe34db3274
Star Bist <malimirkeccita@gmail.com>
2017-01-07 16:21:15 +0100

    Package 1.0.7

--- demo/dist/demo.js
+++ demo/dist/demo.js
@@ -46,81 +46,84 @@ angular.module('RecursionHelper', []).factory('RecursionHelper', ['$compile', fu
 }]);
 angular.module('Repeater', ['RecursionHelper'])
 .directive('recursiveRepeater', ['RecursionHelper', function (RecursionHelper) {
-	return {
-		restrict: 'E',
-		replace: true,
-		scope: {
-			jsonData: '=',
-			label: '='
-		},
-		transclude: true,
-		templateUrl: './templates/repeater.html',
-		compile: function(element) {
-			return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){
-				scope.IsObject = angular.isObject;
-				scope.IsNumber = angular.isNumber;
-            });
-		}
-	};
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: {
+      jsonData: '=',
+      label: '='
+    },
+    transclude: true,
+    templateUrl: './templates/repeater.html',
+    compile: function(element) {
+      return RecursionHelper.compile(element, function(scope, iElement, iAttrs, controller, transcludeFn){
+        scope.IsObject = angular.isObject;
+        scope.IsNumber = angular.isNumber;
+      });
+    }
+  };
 }]);
+
 (function (DemoApp) {
-    'use strict';
+  'use strict';
 
-    DemoApp = angular.module('DemoApp', ['Repeater']);
+  DemoApp = angular.module('DemoApp', ['Repeater']);
 
-    DemoApp.config(function () {});
+  DemoApp.config(function () {});
 }(this));
+
 (function(DemoApp) {
 
-	'use strict';
+  'use strict';
 
-	var controller = ['$scope', '$http', '$q'];
+  var controller = ['$scope', '$http', '$q'];
 
-	controller.push(function($scope, $http, $q) {
-        var loadData = function(url) {
-    		var defer = $q.defer();
-    
-    		if(typeof url !== 'string') {
-    			return false;
-    		}
-    
-    		return $http.get(url).then(function(response) {
-    	 		defer.resolve(response.data);
-    
-    			return defer.promise;
-    		}, function(response) {
-    			defer.reject(response);
-    
-    			return defer.promise;
-    		});
-    	};
-        
-        $scope.myJsonOptions = [{
-            url: './demo/data/dummy.json',
-            label: 'Example A'
-        }, {
-            url: './demo/data/dummy2.json',
-            label: 'Example B'
-        }, {
-            url: './demo/data/dummy3.json',
-            label: 'Example C'
-        }];
-		
-		$scope.myJson = $scope.myJsonOptions[0];
-        
-        var changeData = function() {
-			loadData($scope.myJson.url)
-	        .then(function(data) {
-	            $scope.jsonData = data;
-	        });
-        };
-        
-        $scope.ChangeData = changeData;
-		
-		changeData();
-    });
+  controller.push(function($scope, $http, $q) {
+    var loadData = function(url) {
+      var defer = $q.defer();
 
-	angular.module('DemoApp')
-		.controller('DemoController', controller);
+      if(typeof url !== 'string') {
+        return false;
+      }
+
+      return $http.get(url).then(function(response) {
+        defer.resolve(response.data);
+
+        return defer.promise;
+      }, function(response) {
+        defer.reject(response);
+
+        return defer.promise;
+      });
+    };
+
+    $scope.myJsonOptions = [{
+      url: './demo/data/dummy.json',
+      label: 'Example A'
+    }, {
+      url: './demo/data/dummy2.json',
+      label: 'Example B'
+    }, {
+      url: './demo/data/dummy3.json',
+      label: 'Example C'
+    }];
+
+    $scope.myJson = $scope.myJsonOptions[0];
+
+    var changeData = function() {
+      loadData($scope.myJson.url)
+      .then(function(data) {
+        $scope.jsonData = data;
+      });
+    };
+
+    $scope.ChangeData = changeData;
+
+    changeData();
+  });
+
+  angular.module('DemoApp')
+  .controller('DemoController', controller);
 
 }(this));
+
